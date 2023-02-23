05d875156269ed8fb73e1d80672787ad4e13a1f3
che-97 <rue015@gmail.com>
2023-02-24 01:32:49 +0900

    refactor: 리다이렉트 라우터로 변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,26 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { authContext } from './context/AuthProvider';
 import Layout from './components/common/Layout';
-import TodoProvider from './context/TodoProvider';
-import Root from './pages/Root';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
 
 const Router = () => {
+  const {
+    state: { token },
+  } = useContext(authContext);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Root />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
           <Route
-            path="todo"
-            element={
-              <TodoProvider>
-                <Todo />
-              </TodoProvider>
-            }
+            path="/"
+            exact
+            element={token ? <Navigate to="/todo" replace /> : <SignIn />}
+          />
+          <Route
+            path="/signin"
+            element={token ? <Navigate to="/todo" replace /> : <SignIn />}
+          />
+          <Route
+            path="/signup"
+            element={token ? <Navigate to="/todo" replace /> : <SignUp />}
+          />
+          <Route
+            path="/todo"
+            element={token ? <Todo /> : <Navigate to="/signin" replace />}
+          />
+          <Route
+            path="*"
+            element={token ? <Todo /> : <Navigate to="/signin" replace />}
           />
         </Route>
       </Routes>

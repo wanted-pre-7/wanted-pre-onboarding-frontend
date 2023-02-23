71b3540ace06d1e41e042ba384e57fb867dcd883
ilgon0110 <moses011010@gmail.com>
2023-02-24 02:48:10 +0900

    chore: constant 파일 생성
    ReadOnly 속성을 주기 위해 Object freeze 함수 구현

--- src/constant/formDescription.js
+++ src/constant/formDescription.js
@@ -1,18 +1,27 @@
-export const authDescription = {
-  signin: {
-    emailPlaceholder: '이메일을 입력하세요.',
-    passwordPlaceholder: '비밀번호를 입력하세요.',
-    buttonText: '로그인',
-    actionText: '회원가입',
-    question: '계정이 없으신가요?',
-    actionLink: '/signup',
-  },
-  signup: {
-    emailPlaceholder: '@를 포함한 이메일을 입력하세요.',
-    passwordPlaceholder: '8자 이상 입력하세요.',
-    buttonText: '회원가입',
-    actionText: '로그인',
-    question: '계정이 이미 있으신가요?',
-    actionLink: '/signin',
-  },
+// 읽기 전용 객체로 만들어주는 함수
+const deepFreeze = (targetObj) => {
+  Object.keys(targetObj).forEach((key) => {
+    if (typeof key === 'object') {
+      deepFreeze(targetObj[key]);
+    }
+  });
+  return Object.freeze(targetObj);
 };
+export const AUTH_DESCRIPTION = deepFreeze({
+  '/signin': {
+    EMAIL_PLACEHOLDER: '이메일을 입력하세요.',
+    PASSWORD_PLACEHOLDER: '비밀번호를 입력하세요.',
+    BUTTON_TEXT: '로그인',
+    ACTION_TEXT: '회원가입',
+    QUESTION: '계정이 없으신가요?',
+    ACTION_LINK: '/signup',
+  },
+  '/signup': {
+    EMAIL_PLACEHOLDER: '@를 포함한 이메일을 입력하세요.',
+    PASSWORD_PLACEHOLDER: '8자 이상 입력하세요.',
+    BUTTON_TEXT: '회원가입',
+    ACTION_TEXT: '로그인',
+    QUESTION: '계정이 이미 있으신가요?',
+    ACTION_LINK: '/signin',
+  },
+});
